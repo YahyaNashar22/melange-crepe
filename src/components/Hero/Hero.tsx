9528e71ff2dc2d7a10d2ecab94c529a15b61a177
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-12 17:52:35 +0200

    new hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,9 @@
 import styles from "./Hero.module.css";
 
 import happiness from "../../assets/happiness.jpg";
+import ramy from "../../assets/ramy.jpg";
+
+
 
 const Hero = () => {
   return (
@@ -8,8 +11,16 @@ const Hero = () => {
       <a href="#menu" className={styles.cta}>
         Check Our Menu
       </a>
-      <h1 className={styles.headline}>Unlock your happiness</h1>
-      <h2 className={styles.slogan}>Potential</h2>
+      {/* <h1 className={styles.headline}>Unlock your happiness</h1>
+      <h2 className={styles.slogan}>Potential</h2> */}
+
+<img
+          src={ramy}
+          alt="ramy"
+          className={styles.aboutImg}
+          loading="lazy"
+        />
+
       <img
         src={happiness}
         alt="Delicious Crepes"
